2ca42089bf4145ea96fcbff65769c5cf1d9b7166
toshiaki <conamn1185@gmail.com>
2022-09-12 11:59:36 +0900

    セーブ

--- api/app/controllers/orders_controller.rb
+++ api/app/controllers/orders_controller.rb
@@ -3,6 +3,15 @@ class OrdersController < ApplicationController
   end
 
   def create
+    order = Order.new(customer_id: 1, is_delivered: false,delivery_time: params[:delivery_time])
+    order.save
+    order_details = []
+    params[:item_ids].each do |item_id|
+      order_detail = OrderDetail.new(order_id: order.id, customer_id: 1, item_id: item_id, amount: item_id["amount"])
+      order_detail.save
+      order_details.push(order_detail)
+    end
+    render json: { order: order, order_ids: order_details }, status: :created
   end
 
   def show
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/create'
-  get 'orders/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :customers do
     resources :orders, only: [:index, :create, :show]
