c38075bded7502774c784df2c7e7291da46677a1
honoka2001 <honoka519pk@gmail.com>
2022-09-12 11:05:38 +0900

    fix : OrderDetailにitem_idを追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 2022_09_09_082453) do
   create_table "order_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "order_id", null: false
     t.bigint "customer_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_order_details_on_customer_id"
+    t.index ["item_id"], name: "index_order_details_on_item_id"
     t.index ["order_id"], name: "index_order_details_on_order_id"
   end
 
@@ -48,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_09_09_082453) do
   end
 
   add_foreign_key "order_details", "customers"
+  add_foreign_key "order_details", "items"
   add_foreign_key "order_details", "orders"
   add_foreign_key "orders", "customers"
 end
