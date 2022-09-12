0df27e798295d5e882853ac14228b97b9708cf68
honoka2001 <honoka519pk@gmail.com>
2022-09-12 16:13:39 +0900

    add : customersの予約詳細API

--- api/app/controllers/customers/orders_controller.rb
+++ api/app/controllers/customers/orders_controller.rb
@@ -6,5 +6,16 @@ class Customers::OrdersController < ApplicationController
   end
 
   def show
+    order = Order.find(params[:id])
+    order_details = order.order_details
+    items = []
+    order_details.each do |order_detail|
+      item = order_detail.item
+      amaunt = order_detail.amount
+      order_item = {item: item, amaunt: order_detail.amount}
+      items << order_item
+    end
+    customer = order.customer
+    render json: { id: order.id, is_delivered: order.is_delivered, delivery_time: order.delivery_time, customer: customer, order_details: items, status: :ok }
   end
 end
