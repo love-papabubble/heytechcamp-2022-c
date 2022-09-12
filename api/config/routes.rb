f86bbef8f318385d5678c07384c6cce6588e0a17
toshiaki <conamn1185@gmail.com>
2022-09-12 11:47:00 +0900

    seeder作った

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/create'
+  get 'orders/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :v1 do
-    resources :tasks
+  namespace :customers do
+    resources :orders, only: [:index, :create, :show]
   end
 end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# insert customers
+# 1
+customer = Customer.create!(
+  name: 'Darwin Kusuma',
+  email: 'darwin@gmail.com',
+  password_digest: '123456',
+)
+# 2
+# insert orders
+order = Order.create!(
+  is_delivered: false,
+  delivery_time: '2022-09-30 08:24:53',
+  customer_id: customer.id,
+)
+# insert items
+item = Item.create!(
+  title: 'Burger',
+  description: 'Burger with cheese',
+  price: 10000,
+  item_image: 'https://i.ibb.co/9s0sH1j/burger.png',
+)
+# 2
+# insert order_details
+# 1
+OrderDetail.create!(
+  order_id: order.id,
+  customer_id: customer.id,
+  item_id: item.id,
+)
