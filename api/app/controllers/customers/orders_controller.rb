d6b30b99571c5488364c060384c312edd1bd330c
honoka2001 <honoka519pk@gmail.com>
2022-09-13 11:14:08 +0900

    add : 合計金額の計算

--- api/app/controllers/customers/orders_controller.rb
+++ api/app/controllers/customers/orders_controller.rb
@@ -8,14 +8,20 @@ class Customers::OrdersController < ApplicationController
   def show
     order = Order.find(params[:id])
     order_details = order.order_details
+    customer = order.customer
+
     items = []
+    sum_price = 0
+
     order_details.each do |order_detail|
       item = order_detail.item
-      amaunt = order_detail.amount
-      order_item = {item: item, amaunt: order_detail.amount}
+      amount = order_detail.amount
+      order_item = {item: {id: item.id, title: item.title, description: item.description.truncate(20), price: item.price, item_image: item.item_image}, amount: order_detail.amount}
       items << order_item
+
+      sum_price += amount * item.price
     end
-    customer = order.customer
-    render json: { id: order.id, is_delivered: order.is_delivered, delivery_time: order.delivery_time, customer: customer, order_details: items, status: :ok }
+
+    render json: { id: order.id, is_delivered: order.is_delivered, sum_price: sum_price, delivery_time: order.delivery_time, customer: customer, order_details: items, status: :ok }
   end
 end
