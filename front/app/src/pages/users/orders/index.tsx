0a58802441bfeefa9c1dded87ae027e688882020
toshiaki <conamn1185@gmail.com>
2022-09-14 11:54:11 +0900

    feat:ネットワーク断絶時に備えて店舗側の注文はlocalstorageに保存した

--- front/app/src/pages/users/orders/index.tsx
+++ front/app/src/pages/users/orders/index.tsx
@@ -10,10 +10,22 @@ const Home: NextPage = () => {
     getOrders();
   }, []);
 
+  //function to store orders in the localstorage
+    const storeOrders = (orders: OrderInfo[]) => {
+        localStorage.setItem('orders', JSON.stringify(orders));
+    }
+
   async function getOrders() {
     try {
       const res = await axios.get(`http://localhost:3000/users/orders/`);
+      //if backend access failed, get orders from localstorage
+        if (res.status !== 200) {
+            const orders = JSON.parse(localStorage.getItem('orders') || '[]');
+            setOrders(orders);
+            return;
+        }
       console.log(res);
+      storeOrders(res.data);
       setOrders(res.data);
     } catch (err) {
       console.log(err);
