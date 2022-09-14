c033297fe3f1e43cd10aeb6e26495ecae0c7333e
toshiaki <conamn1185@gmail.com>
2022-09-14 15:31:59 +0900

    feat:カメラ起動

--- front/app/src/pages/users/orders/index.tsx
+++ front/app/src/pages/users/orders/index.tsx
@@ -1,42 +1,56 @@
+import {Button, Container} from '@mui/material';
 import axios from 'axios';
-import type { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
-import { OrderInfo } from '@/components/Orders/index';
-import { OrderList } from '@/components/Orders/users/OrderList';
+import type {NextPage} from 'next';
+import React, {useEffect, useState} from 'react';
+import {OrderInfo} from '@/components/Orders';
+import {OrderList} from '@/components/Orders/users/OrderList';
+import {QRCodeReader} from "@/pages/users/orders/QRCodeReader";
 
 const Home: NextPage = () => {
-  const [orders, setOrders] = useState<OrderInfo[]>([]);
-  useEffect(() => {
-    getOrders();
-  }, []);
+    const [orders, setOrders] = useState<OrderInfo[]>([]);
+    const [isQRCameraOpen, setIsQRCameraOpen] = useState(false);
 
-  //function to store orders in the localstorage
+    const openQRCamera = () => {
+        setIsQRCameraOpen(true);
+    }
+
+    useEffect(() => {
+        getOrders();
+    }, []);
+
+    //function to store orders in the localstorage
     const storeOrders = (orders: OrderInfo[]) => {
         localStorage.setItem('orders', JSON.stringify(orders));
     }
 
-  async function getOrders() {
-    try {
-      const res = await axios.get(`http://localhost:3000/users/orders/`);
-      //if backend access failed, get orders from localstorage
-        if (res.status !== 200) {
-            const orders = JSON.parse(localStorage.getItem('orders') || '[]');
-            setOrders(orders);
-            return;
+    async function getOrders() {
+        try {
+            const res = await axios.get(`http://localhost:3000/users/orders/`);
+            //if backend access failed, get orders from localstorage
+            if (res.status !== 200) {
+                const orders = JSON.parse(localStorage.getItem('orders') || '[]');
+                setOrders(orders);
+                return;
+            }
+            console.log(res);
+            storeOrders(res.data);
+            setOrders(res.data);
+        } catch (err) {
+            console.log(err);
         }
-      console.log(res);
-      storeOrders(res.data);
-      setOrders(res.data);
-    } catch (err) {
-      console.log(err);
     }
-  }
 
-  return (
-    <>
-      <OrderList ordersInfo={orders} />
-    </>
-  );
+    return (
+        <>
+            <Container maxWidth="md" style={{paddingTop: "1em", paddingBottom: "10em"}}>
+                <Button onClick={() => openQRCamera()}>test</Button>
+                {isQRCameraOpen && <QRCodeReader onRead={(r) => {
+                    setIsQRCameraOpen(false);
+                }} setOpen={setIsQRCameraOpen}/>}
+            </Container>
+            <OrderList ordersInfo={orders}/>
+        </>
+    );
 };
 
 export default Home;
