ca3ff99a18a0eca3b0ad29b916b41f497706175d
Ebi-web <74973675+Ebi-web@users.noreply.github.com>
2022-09-15 13:10:59 +0900

    refactor:QRコードリーダーをページ化した (#15)

--- front/app/src/pages/users/orders/index.tsx
+++ front/app/src/pages/users/orders/index.tsx
@@ -1,18 +1,14 @@
-import {Button, Container} from '@mui/material';
+import {Container} from '@mui/material';
 import axios from 'axios';
 import type {NextPage} from 'next';
+import Link from "next/link";
 import React, {useEffect, useState} from 'react';
 import {OrderInfo} from '@/components/Orders';
 import {OrderList} from '@/components/Orders/users/OrderList';
-import {QRCodeReader} from "@/pages/users/orders/QRCodeReader";
+import {useRouter} from "next/router";
 
 const Home: NextPage = () => {
     const [orders, setOrders] = useState<OrderInfo[]>([]);
-    const [isQRCameraOpen, setIsQRCameraOpen] = useState(false);
-
-    const openQRCamera = () => {
-        setIsQRCameraOpen(true);
-    }
 
     useEffect(() => {
         getOrders();
@@ -44,11 +40,7 @@ const Home: NextPage = () => {
         <>
             <OrderList ordersInfo={orders}/>
             <Container maxWidth="md" style={{paddingTop: "1em", paddingBottom: "10em"}}>
-                <Button onClick={() => openQRCamera()}>QRコードリーダーを開く</Button>
-                {isQRCameraOpen && <QRCodeReader onRead={(r) => {
-                    setIsQRCameraOpen(false);
-                    window.location.href = r.getText();
-                }} setOpen={setIsQRCameraOpen}/>}
+                <Link href={"/users/orders/qr-code-reader"}>QRコードを読み取る</Link>
             </Container>
         </>
     );
