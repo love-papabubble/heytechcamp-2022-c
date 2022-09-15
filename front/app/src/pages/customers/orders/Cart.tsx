5d23498fc5f9b9d3bb4be4b2df9414e76c51b86c
Ebi-web <74973675+Ebi-web@users.noreply.github.com>
2022-09-15 15:03:26 +0900

    style:注文の商品と注文パネルは並列に表示 (#21)
    * style:注文の商品と注文パネルは並列に表示
    
    * run prettier

--- front/app/src/pages/customers/orders/Cart.tsx
+++ front/app/src/pages/customers/orders/Cart.tsx
@@ -73,7 +73,7 @@ const Cart: FC<CartProps> = (props: CartProps) => {
 
   return (
     <>
-      <Container fixed>
+      <Container fixed style={{ flexBasis: '30%' }}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <Stack spacing={3}>
             <Typography variant='h5' component='div'>
@@ -100,7 +100,7 @@ const Cart: FC<CartProps> = (props: CartProps) => {
                     <Typography variant='h5' component='div' key={index}>
                       {cartDetail.item.title} x {cartDetail.quantity}
                     </Typography>
-                    <Typography variant='h5' component='div' key={index}>
+                    <Typography variant='h5' component='div'>
                       小計:{cartDetail.item.price * cartDetail.quantity}円
                     </Typography>
                   </>
